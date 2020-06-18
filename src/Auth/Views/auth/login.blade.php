6e74525e82e9b54e0b7533ea3d1ae576e62bc39a
fadighattas100 <fadighattas100@gmail.com>
2020-06-18 19:35:48 +0200

    Update LoginController, add terms and conditions validation

--- src/Auth/Views/auth/login.blade.php
+++ src/Auth/Views/auth/login.blade.php
@@ -15,7 +15,7 @@
                                 <input id="username" type="username" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" required autocomplete="username" autofocus>
 
                                 @error('username')
-                                <span class="invalid-feedback" role="alert">
+                                    <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
@@ -29,7 +29,7 @@
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
-                                <span class="invalid-feedback" role="alert">
+                                    <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
@@ -43,18 +43,6 @@
 
                                     <label class="form-check-label" for="remember">
                                         {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="tac" id="tac">
-
-                                    <label class="form-check-label" for="tac">
-                                        {{ __('I agree to terms and conditions') }}
                                     </label>
                                 </div>
                             </div>
